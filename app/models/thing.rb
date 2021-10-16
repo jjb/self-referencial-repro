0ad5fe17ca8fdb385dab018746b2440c3e51710a
John Bachir <j@jjb.cc>
2021-10-16 18:35:18 +0200

    comments with fix

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,4 +1,4 @@
 class Thing < ApplicationRecord
-  belongs_to :thing, required: false
-  has_many :things
+  belongs_to :thing, required: false#, inverse_of: :things
+  has_many :things#, inverse_of: :thing
 end
